6aa7fa1fab077048b0803ef9472bb9206bfdb97e
AzwadFawadHasan <106096161+AzwadFawadHasan@users.noreply.github.com>
2025-08-11 12:33:55 +0600

    test push

--- src/app/(admin)/doctor/patient-history/list/see-patient-history/[id]/page.tsx
+++ src/app/(admin)/doctor/patient-history/list/see-patient-history/[id]/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 interface PatientInfo {
   patient_id: number;
   patient_name: string;
+  // email: string;
   email: string;
   mobile_number: string;
   gender: string;
